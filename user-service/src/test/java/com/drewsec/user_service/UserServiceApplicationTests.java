052b15bf231aa416d33ef19a5d2ab288948da892
novmah62 <at180532@actvn.edu.vn>
2025-04-12 21:54:28 +0700

    feat(core-microservice): done discovery server, config server

---
+++ user-service/src/test/java/com/drewsec/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drewsec.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
