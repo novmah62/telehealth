2f99f3fb56b7dff7695e67c20e201e8e710d5522
novmah62 <at180532@actvn.edu.vn>
2025-04-26 14:30:35 +0700

    feat(base-domain): setup database and test run

---
+++ document/db/init-mongo.js
@@ -0,0 +1,23 @@
+// Consultation Service
+db = db.getSiblingDB("consultation_service");
+// db.createUser({
+//     user: "consultation_user",
+//     pwd: "consultation_pass",
+//     roles: [{ role: "readWrite", db: "consultation_service" }]
+// });
+
+// Audit Service
+db = db.getSiblingDB("audit_service");
+// db.createUser({
+//     user: "audit_user",
+//     pwd: "audit_pass",
+//     roles: [{ role: "readWrite", db: "audit_service" }]
+// });
+
+// Notification Service
+db = db.getSiblingDB("notification_service");
+// db.createUser({
+//     user: "notification_user",
+//     pwd: "notification_pass",
+//     roles: [{ role: "readWrite", db: "notification_service" }]
+// });
---
+++ document/db/init-postgres.sql
@@ -0,0 +1,25 @@
+-- =============================
+-- Create databases
+-- =============================
+CREATE DATABASE user_service;
+CREATE DATABASE appointment_service;
+CREATE DATABASE prescription_service;
+CREATE DATABASE verification_service;
+
+-- -- =============================
+-- -- Create users & grant privileges for user_service
+-- -- =============================
+-- CREATE USER user_svc_user WITH PASSWORD 'user_svc_pass';
+-- GRANT ALL PRIVILEGES ON DATABASE user_service TO user_svc_user;
+--
+-- \connect appointment_service
+-- CREATE USER appointment_svc_user WITH PASSWORD 'appointment_svc_pass';
+-- GRANT ALL PRIVILEGES ON DATABASE appointment_service TO appointment_svc_user;
+--
+-- \connect prescription_service
+-- CREATE USER prescription_svc_user WITH PASSWORD 'prescription_svc_pass';
+-- GRANT ALL PRIVILEGES ON DATABASE prescription_service TO prescription_svc_user;
+--
+-- \connect verification_service
+-- CREATE USER verification_svc_user WITH PASSWORD 'verification_svc_pass';
+-- GRANT ALL PRIVILEGES ON DATABASE verification_service TO verification_svc_user;
