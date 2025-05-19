40ab4adab9fc0c6b781357878bd3e139638d788a
novmah62 <at180532@actvn.edu.vn>
2025-05-19 22:27:56 +0700

    feat(prescription-service): add mapper

--- prescription-service/src/main/java/com/drewsec/prescription_service/entity/DigitalSignature.java
+++ prescription-service/src/main/java/com/drewsec/prescription_service/entity/DigitalSignature.java
@@ -3,13 +3,12 @@ package com.drewsec.prescription_service.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Lob;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
 public class DigitalSignature {
 
     @Lob
